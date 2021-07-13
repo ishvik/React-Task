e0d29167d85a1a3a58babd13dc9db10f6ee6fbd7
Simran Singh <singh.simran9654@gmail.com>
2021-07-13 12:00:37 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA5arDM46C9v6zvQpdP5s12L_7PN7Kd6oI",
+    authDomain: "campaign-20ce2.firebaseapp.com",
+    projectId: "campaign-20ce2",
+    storageBucket: "campaign-20ce2.appspot.com",
+    messagingSenderId: "767741736030",
+    appId: "1:767741736030:web:ab4646c6783cb81c5d30cf"
+})
+
+export default firebase;
